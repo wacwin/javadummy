8b929417c64ca6eca5867db0287399f9b01d48d4
Wacle <wacwin.outlook.com>
2016-02-21 11:44:05 +0800

    Update DateTest to show date format conversion

--- DateTest/DateTest.java
+++ DateTest/DateTest.java
@@ -1,9 +1,15 @@
 import java.util.Date;
+import java.text.SimpleDateFormat;
 
 public class DateTest{
   public static void main(String[] args){
     Date now = new Date();
     System.out.println("Today is:" + now);
     System.out.println(now.getTime());
+
+    // Format a date in a given format
+    SimpleDateFormat dateFormatter = new SimpleDateFormat("MM/dd/yyyy hh:mm:ss");
+    String newDateString = dateFormatter.format(now);
+    System.out.println(newDateString);
   }
 }
